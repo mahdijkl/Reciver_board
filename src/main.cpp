a5f5d44ce240d8b9a729e4b118757183efa80784
Mahdi <mohammadmahdi.org@gmail.com>
2024-07-23 22:10:11 +0330

    Fix some bugs.

--- src/main.cpp
+++ src/main.cpp
@@ -8,64 +8,28 @@
 #include "LittleFS.h"
 #include <queue>
 
-// const char *ssidChar = "Joshani";
-// const char *passwordChar = "Arefi1400#";
+AsyncWebServer server(80);
 
 uint8_t sensorNodeMacAddress[] = {0xD4, 0x8A, 0xFC, 0x9F, 0x6B, 0x74};
 int retryEspNowCount = 0;
 
-// const char *PARAM_INPUT_1 = "ssid";
-// const char *PARAM_INPUT_2 = "pass";
-// const char *PARAM_INPUT_3 = "ip";
-// const char *PARAM_INPUT_4 = "gateway";
-// const char *PARAM_INPUT_5 = "serverUrl";
-// const char *PARAM_INPUT_6 = "apikey";
-// const char *PARAM_INPUT_7 = "telegramApiToken";
-// const char *PARAM_INPUT_8 = "telegramChatId";
-
-// Variables to save values from HTML form
 String ssid;
 String pass;
-String ip;
-String gateway;
 String serverUrl;
 String apiKey;
 String telegramApiToken;
 String telegramChatId;
 String telegramUrl;
 
-// File paths to save input values permanently
-const char *ssidPath = "/ssid.txt";
-const char *passPath = "/pass.txt";
-const char *ipPath = "/ip.txt";
-const char *gatewayPath = "/gateway.txt";
-const char *serverUrlPath = "/serverUrl.txt";
-const char *apikeyPath = "/apikey.txt";
-const char *telegramApiTokenPath = "/telegramApiToken.txt";
-const char *telegramChatIdPath = "/telegramChatId.txt";
-
-IPAddress localIP;
-// IPAddress localIP(192, 168, 1, 200); // hardcoded
-
-// Set your Gateway IP address
-IPAddress localGateway;
-// IPAddress localGateway(192, 168, 1, 1); //hardcoded
-IPAddress subnet(255, 255, 0, 0);
-
 // Timer variables
 unsigned long previousMillis = 0;
 const long interval = 10000; // interval to wait for Wi-Fi connection (milliseconds)
-
-// String apiKey = "16d7a4fca7442dda3ad93c9a726597e4";
-// String serverUrl = "http://188.213.196.180:8007/api/motion";
-
-// String telegramApiToken = "188428767:AEhcC4SpIDyZbMHeqgU8OAWeaJGF4KqzGIFGDVVn";
-// String telegramChatId = "1292108244";
 
 bool motionDetected = false;
 bool motionStopped = false;
 
 const int LED_PIN = 19;
+const int BUTTON_PIN = 12;
 
 typedef struct struct_message
 {
@@ -138,6 +102,7 @@ void writeFile(fs::FS &fs, const char *path, const char *message)
   }
 }
 
+// Delete File from LittleFS
 void deleteFile(fs::FS &fs, const char *path)
 {
   Serial.printf("Deleting file: %s\r\n", path);
@@ -258,6 +223,8 @@ void setupESPNow()
     Serial.println("Failed to add peer");
     return;
   }
+  Serial.print("WiFi channel: ");
+  Serial.println(WiFi.channel());
 
   myData.wifiChannel = WiFi.channel();
   esp_err_t result = esp_now_send(sensorNodeMacAddress, (uint8_t *)&myData, sizeof(myData));
@@ -273,7 +240,6 @@ void setupESPNow()
   }
 }
 
-// Initialize WiFi
 bool initWiFi()
 {
   if (ssid == "")
@@ -308,7 +274,6 @@ bool initWiFi()
 
 void initialSetup()
 {
-  AsyncWebServer server(80);
 
   Serial.println("Setting AP (Access Point)");
 
@@ -318,7 +283,6 @@ void initialSetup()
   Serial.print("AP IP address: ");
   Serial.println(IP);
 
-  // Web Server Root URL
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(LittleFS, "/wifimanager.html", "text/html"); });
 
@@ -330,70 +294,47 @@ void initialSetup()
       for(int i=0;i<params;i++){
         AsyncWebParameter* p = request->getParam(i);
         if(p->isPost()){
-          // HTTP POST ssid value
+
           if (p->name() == "ssid") {
             ssid = p->value().c_str();
             Serial.print("SSID set to: ");
             Serial.println(ssid);
-            // Write file to save value
-            writeFile(LittleFS, ssidPath, ssid.c_str());
+            writeFile(LittleFS, "/ssid.txt", ssid.c_str());
           }
-          // HTTP POST pass value
+
           if (p->name() == "pass") {
             pass = p->value().c_str();
             Serial.print("Password set to: ");
             Serial.println(pass);
-            // Write file to save value
-            writeFile(LittleFS, passPath, pass.c_str());
-          }
-          // HTTP POST ip value
-          if (p->name() == "ip") {
-            ip = p->value().c_str();
-            Serial.print("IP Address set to: ");
-            Serial.println(ip);
-            // Write file to save value
-            writeFile(LittleFS, ipPath, ip.c_str());
-          }
-          // HTTP POST gateway value
-          if (p->name() == "gateway") {
-            gateway = p->value().c_str();
-            Serial.print("Gateway set to: ");
-            Serial.println(gateway);
-            // Write file to save value
-            writeFile(LittleFS, gatewayPath, gateway.c_str());
+            writeFile(LittleFS, "/pass.txt", pass.c_str());
           }
           if (p->name() == "serverUrl") {
             serverUrl = p->value().c_str();
             Serial.print("serverUrl set to: ");
             Serial.println(serverUrl);
-            // Write file to save value
-            writeFile(LittleFS, serverUrlPath, serverUrl.c_str());
+            writeFile(LittleFS, "/serverUrl.txt", serverUrl.c_str());
           }
           if (p->name() == "apikey") {
             apiKey = p->value().c_str();
             Serial.print("apiKey set to: ");
             Serial.println(apiKey);
-            // Write file to save value
-            writeFile(LittleFS, apikeyPath, apiKey.c_str());
+            writeFile(LittleFS, "/apikey.txt", apiKey.c_str());
           }
           if (p->name() == "telegramApiToken") {
             telegramApiToken = p->value().c_str();
             Serial.print("telegramApiToken set to: ");
             Serial.println(telegramApiToken);
-            // Write file to save value
-            writeFile(LittleFS, telegramApiTokenPath, telegramApiToken.c_str());
+            writeFile(LittleFS, "/telegramApiToken.txt", telegramApiToken.c_str());
           }
           if (p->name() == "telegramChatId") {
             telegramChatId = p->value().c_str();
             Serial.print("telegramChatId set to: ");
             Serial.println(telegramChatId);
-            // Write file to save value
-            writeFile(LittleFS, telegramChatIdPath, telegramChatId.c_str());
+            writeFile(LittleFS, "/telegramChatId.txt", telegramChatId.c_str());
           }
-          //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
         }
       }
-      request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
+      request->send(200, "text/plain", "Done. ESP will restart.");
       delay(3000);
       ESP.restart(); });
   server.begin();
@@ -401,27 +342,28 @@ void initialSetup()
 
 void loadValue()
 {
-  ssid = readFile(LittleFS, ssidPath);
-  pass = readFile(LittleFS, passPath);
-  ip = readFile(LittleFS, ipPath).c_str();
-  gateway = readFile(LittleFS, gatewayPath).c_str();
-  serverUrl = readFile(LittleFS, serverUrlPath).c_str();
-  apiKey = readFile(LittleFS, apikeyPath).c_str();
-  telegramApiToken = (String)readFile(LittleFS, telegramApiTokenPath);
-  telegramChatId = (String)readFile(LittleFS, telegramChatIdPath);
-  Serial.println(ssid);
-  Serial.println(pass);
-  Serial.println(ip);
-  Serial.println(serverUrl);
-  Serial.println(apiKey);
-  Serial.println(telegramApiToken);
-  Serial.println(telegramChatId);
+  ssid = readFile(LittleFS, "/ssid.txt");
+  pass = readFile(LittleFS, "/pass.txt");
+  serverUrl = readFile(LittleFS, "/serverUrl.txt");
+  apiKey = readFile(LittleFS, "/apikey.txt");
+  telegramApiToken = readFile(LittleFS, "/telegramApiToken.txt");
+  telegramChatId = readFile(LittleFS, "/telegramChatId.txt");
+}
+
+void resetModule()
+{
+  deleteFile(LittleFS, "/ssid.txt");
+  deleteFile(LittleFS, "/pass.txt");
+  deleteFile(LittleFS, "/serverUrl.txt");
+  deleteFile(LittleFS, "/apikey.txt");
+  ESP.restart();
 }
 
 void setup()
 {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
   initLittleFS();
   loadValue();
 
@@ -437,6 +379,10 @@ void setup()
 
 void loop()
 {
+  if (digitalRead(BUTTON_PIN) == HIGH)
+  {
+    resetModule();
+  }
   while (!messageQueue.empty())
   {
     struct_message msg = messageQueue.front();
